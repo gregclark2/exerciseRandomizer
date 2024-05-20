1984ed94649d1800896288a1c90a5149f8d088b7
gclark2 <gclark2@my.chattanoogastate.edu>
2024-05-20 11:10:57 -0400

    new exercise entries

--- Main.java
+++ Main.java
@@ -11,13 +11,22 @@ public class Main {
         HashMap<Integer, Exercise> exercises = new HashMap<Integer, Exercise>();
 
         //exercise objects
-        Exercise airSquats = new Exercise("Air Squats", 1, "5");
-        Exercise pushUps = new Exercise("Push Ups", 2, "2");
+        Exercise airSquats = new Exercise("Air Squats", 1, "10 reps");
+        Exercise pushUps = new Exercise("Push Ups", 2, "2 reps");
+        Exercise lSit = new Exercise("L Sit Practice", 3, "60 seconds");
+        Exercise lLifts = new Exercise("Leg Lift Variation", 4, "20");
+        Exercise iRow = new Exercise("Inverted Ring Row", 5, "15");
 
         //add to hashmap - skipping index 0 for convenience
         exercises.put(1, airSquats);
         exerciseCount++;
         exercises.put(2, pushUps);
+        exerciseCount++;
+        exercises.put(3, lSit);
+        exerciseCount++;
+        exercises.put(4, lLifts);
+        exerciseCount++;
+        exercises.put(5, iRow);
         exerciseCount++;
 
         System.out.println("Time to move? (y/n)");
